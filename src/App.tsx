9e50437cf66549e553e697040391d5caa4d23e22
YEONSEO <yeonseochoi0717@gmail.com>
2025-06-04 19:53:06 +0900

    feat: 구글, 깃허브, 이메일 인증 회원가입 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+import supabase from './utils/supabase';
+
 export default function App() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleGoogleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+  };
+
+  const handleGithubLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    });
+  };
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          name: name,
+          avatar_url: null,
+        },
+      },
+    });
+  };
   return (
     <>
-      <h1>App Component</h1>
+      <button onClick={handleGoogleLogin}>Google 로그인</button>
+      <br />
+      <button onClick={handleGithubLogin}>Github 로그인</button>
+      <br />
+      <form>
+        <input
+          type="text"
+          placeholder="이름을 입력하세요"
+          onChange={(e) => setName((e.target as HTMLInputElement).value)}
+        />
+        <input
+          type="email"
+          placeholder="이메일을 입력하세요"
+          onChange={(e) => setEmail((e.target as HTMLInputElement).value)}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호을 입력하세요"
+          onChange={(e) => setPassword((e.target as HTMLInputElement).value)}
+        />
+        <button type="submit" onClick={handleSignUp}>
+          가입하기
+        </button>
+      </form>
     </>
   );
 }
