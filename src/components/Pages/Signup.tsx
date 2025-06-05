c5644afd652320a0c5feecb93df0b9f0dde72669
dlakrp123 <dlakrp731@gmail.com>
2025-06-06 01:15:56 +0900

    Feat: 모바일 signup 구현, LoginButton atom추가, 모달 추가가

--- src/components/Pages/Signup.tsx
+++ src/components/Pages/Signup.tsx
@@ -1,35 +1,72 @@
+import { useState } from 'react';
+import { Check, Eye, EyeOff, Lock, Mail, User } from 'lucide-react';
 import LoginButton from '../atoms/LoginButton';
+import EmailVerificationModal from '../modals/EmailVertificationModal';
 
 export default function Signup() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="bg-bg-white flex min-h-screen items-center justify-center px-4">
-      <div className="w-full max-w-xs">
-        <h1 className="text-main h4 mb-6 text-center !font-bold">
+    <div className="bg-bg-white flex min-h-screen items-center justify-center">
+      <div className="flex w-full max-w-xs flex-col items-center">
+        <h1 className="text-main h4 mb-[26px] text-center !font-bold">
           Create Your Account
         </h1>
-        <input
-          className="t5 border-gray3 mb-[16px] w-full rounded-[4px] border bg-white px-4 py-2"
-          placeholder="이름을 입력하세요"
-        />
-        <input
-          type="email"
-          className="t5 border-gray3 mb-[16px] w-full rounded-[4px] border bg-white px-4 py-2"
-          placeholder="이메일 주소를 입력하세요"
-        />
-        <input
-          type="password"
-          className="t5 border-gray3 mb-[20px] w-full rounded-[4px] border bg-white px-4 py-2"
-          placeholder="비밀번호를 입력하세요"
-        />
-        <button className="bg-main mb-[30px] w-full rounded-[4px] py-2 text-sm font-semibold text-white">
+
+        <div className="relative mb-[16px]">
+          <User className="text-gray3 absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2" />
+          <input
+            className="t5 border-gray3 placeholder:t5 h-[32px] w-[260px] rounded-[4px] border bg-white pl-10"
+            placeholder="이름을 입력하세요"
+          />
+        </div>
+
+        <div className="relative mb-[16px]">
+          <Mail className="text-gray3 absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2" />
+          <Check className="text-green-info absolute top-1/2 right-3 h-5 w-5 -translate-y-1/2" />
+          <input
+            type="email"
+            className="t5 border-gray3 placeholder:t5 h-[32px] w-[260px] rounded-[4px] border bg-white pl-10"
+            placeholder="이메일 주소를 입력하세요"
+          />
+        </div>
+
+        <div className="relative mb-[20px]">
+          <Lock className="text-gray3 absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2" />
+          {showPassword ? (
+            <EyeOff
+              className="text-gray4 absolute top-1/2 right-3 h-5 w-5 -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <Eye
+              className="text-gray4 absolute top-1/2 right-3 h-5 w-5 -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+          <input
+            type={showPassword ? 'text' : 'password'}
+            className="t5 border-gray3 placeholder:t5 h-[32px] w-[260px] rounded-[4px] border bg-white pl-10"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-main t3 mb-[30px] h-[32px] w-[260px] cursor-pointer rounded-[4px] text-white transition hover:shadow-lg active:bg-black"
+        >
           가입하기
         </button>
-        <div className="t5 mb-[16px] text-sm">
+        <EmailVerificationModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+        <div className="t5 mb-[12px] w-[260px] text-left">
           이미 계정이 있으신가요?{' '}
-          <span className="text-sub1 font-bold">로그인</span>
+          <button className="text-sub1 cursor-pointer font-bold">로그인</button>
         </div>
         <div className="mb-[40px] flex items-center justify-center">
-          <span className="t1 text-gray3 mx-2 text-sm !font-bold">- OR -</span>
+          <span className="t1 text-gray3 !font-bold">- OR -</span>
         </div>
         <LoginButton />
       </div>
--- src/components/atoms/LoginButton.tsx
+++ src/components/atoms/LoginButton.tsx
@@ -1,8 +1,24 @@
 export default function LoginButton() {
   return (
-    <>
-      <div>Log in with Google</div>
-      <div>Log in whit Github</div>
-    </>
+    <div className="flex justify-center">
+      <div className="space-y-[20px]">
+        <div className="t3 relative flex h-[40px] w-[220px] cursor-pointer items-center rounded-[4px] bg-white text-[#7c838a] drop-shadow-sm">
+          <img
+            src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+            alt="Google"
+            className="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2"
+          />
+          <span className="flex-1 text-center">Log in with Google</span>
+        </div>
+        <div className="t3 relative flex h-[40px] w-[220px] cursor-pointer items-center rounded-[4px] bg-white text-[#7c838a] drop-shadow-sm">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+            alt="Github"
+            className="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2"
+          />
+          <span className="flex-1 text-center">Log in with Github</span>
+        </div>
+      </div>
+    </div>
   );
 }
