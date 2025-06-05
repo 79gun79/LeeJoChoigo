a15cdcaa836eecade78a1f40832a1873098e58f8
dlakrp123 <dlakrp731@gmail.com>
2025-06-05 15:23:56 +0900

    feat: add Header component to MainLayout for improved layout structure

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router';
 export default function AuthLayout() {
   return (
     <>
-      <Header />
       <main>
         <Outlet />
       </main>
--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 export default function MainLayout() {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
