40177c51f5297630cf7f782ce1445b4f13a4b334
dlakrp123 <dlakrp731@gmail.com>
2025-06-08 23:30:33 +0900

    Feat: 문제게시글 상세 ui 및 버튼 컴포넌트 추가가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 
 import './styles/index.css';
 import Router from './routes';
+import JobDetailedPage from './pages/job/JobDetailedPage';
+import AlgorithmSolutionEdit from './pages/solution/algorithm/AlgorithmSolutionEdit';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <JobDetailedPage />
   </StrictMode>,
 );
