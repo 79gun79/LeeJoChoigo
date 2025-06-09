1bd649fb5130511c0c42690c9ce804c9ec9aeef1
79gun79 <79gun79@gmail.com>
2025-06-09 18:07:10 +0900

    main 원래대로

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router';
 import Header from './Header';
-import JobDetailedPage from '../pages/problem/quiz/JobDetailedPage';
 
 export default function MainLayout() {
   return (
     <>
       <Header />
       <main>
-        <JobDetailedPage />
         <Outlet />
       </main>
     </>
