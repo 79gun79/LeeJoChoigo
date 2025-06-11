40a34bab599f0febb688f24afa4f4827c4f315c9
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-06-11 15:51:19 +0900

    fix: devtools 제거

--- src/stores/problemStore.ts
+++ src/stores/problemStore.ts
@@ -1,7 +1,6 @@
 import { create } from 'zustand';
 import type { PostType } from '../types/post';
 import { fetchBjProblems } from '../utils/fetchBjProblems';
-import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 type PostStore = {
@@ -10,22 +9,19 @@ type PostStore = {
 };
 
 export const useProblemStore = create<PostStore>()(
-  devtools(
-    immer((set, get) => ({
-      problems: [],
-      setProblemsByPage: async (page: number) => {
-        const exists = get().problems.find((p) => p.page === page);
-        if (exists) return;
+  immer((set, get) => ({
+    problems: [],
+    setProblemsByPage: async (page: number) => {
+      const exists = get().problems.find((p) => p.page === page);
+      if (exists) return;
 
-        const data = await fetchBjProblems(page);
-        if (data && data.length > 0) {
-          set((state) => {
-            state.problems.push({ page, posts: data });
-          });
-        }
-        console.log('저장완료 : ', get().problems.length);
-      },
-    })),
-    { name: 'ProblemStore' },
-  ),
+      const data = await fetchBjProblems(page);
+      if (data && data.length > 0) {
+        set((state) => {
+          state.problems.push({ page, posts: data });
+        });
+      }
+      console.log('저장완료 : ', get().problems.length);
+    },
+  })),
 );
