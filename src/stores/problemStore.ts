a5afea19a4120ec5028e3caceb06922d32ca317e
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-06-11 16:19:17 +0900

    fix: 타입 에러 수정

--- src/stores/problemStore.ts
+++ src/stores/problemStore.ts
@@ -1,27 +1,25 @@
 import { create } from 'zustand';
 import type { PostType } from '../types/post';
 import { fetchBjProblems } from '../utils/fetchBjProblems';
-import { immer } from 'zustand/middleware/immer';
+import { devtools } from 'zustand/middleware';
 
 type PostStore = {
   problems: { page: number; posts: PostType[] }[];
   setProblemsByPage: (page: number) => Promise<void>;
 };
 
-export const useProblemStore = create<PostStore>()(
-  immer((set, get) => ({
-    problems: [],
-    setProblemsByPage: async (page: number) => {
-      const exists = get().problems.find((p) => p.page === page);
-      if (exists) return;
+export const useProblemStore = create<PostStore>((set, get) => ({
+  problems: [],
+  setProblemsByPage: async (page: number) => {
+    const exists = get().problems.find((p) => p.page === page);
+    if (exists) return;
 
-      const data = await fetchBjProblems(page);
-      if (data && data.length > 0) {
-        set((state) => {
-          state.problems.push({ page, posts: data });
-        });
-      }
-      console.log('저장완료 : ', get().problems.length);
-    },
-  })),
-);
+    const data = await fetchBjProblems(page);
+    if (data && data.length > 0) {
+      set((state) => ({
+        problems: [...state.problems, { page, posts: data }],
+      }));
+    }
+    console.log('저장완료 : ', get().problems.length + '개');
+  },
+}));
