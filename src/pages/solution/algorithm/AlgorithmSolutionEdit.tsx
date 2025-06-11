c63d20884dd83dfb2f62fce7449b364c23580a99
CHOIYEONSEO <yeonseochoi0717@gmail.com>
2025-06-11 17:44:37 +0900

    feat: 알고리즘 문제 게시판에서 풀이 페이지로 연결

--- src/pages/solution/algorithm/AlgorithmSolutionEdit.tsx
+++ src/pages/solution/algorithm/AlgorithmSolutionEdit.tsx
@@ -1,11 +1,16 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState, useTransition } from 'react';
 import EditText from '../../../components/edit/EditText';
 import PageName from '../../../components/ui/PageName';
 import type { EditTextHandle } from '../../../components/edit/EditText.types';
+import { useParams } from 'react-router';
+// import { fetchBjProblemById } from '../../../utils/fetchBjProblems';
 
 export default function AlgorithmSolutionEdit() {
   const [tags, setTags] = useState<string[]>([]);
   const editTextRef = useRef<EditTextHandle>(null);
+  const params = useParams<string>().id;
+  const [isLoading, startTransition] = useTransition();
+  const [problem, setProblem] = useState();
 
   const handleAddTag = (tag: string) => {
     if (!tags.includes(tag)) {
@@ -16,12 +21,21 @@ export default function AlgorithmSolutionEdit() {
   const handleRemoveTag = (tag: string) => {
     setTags((prev) => prev.filter((t) => t !== tag));
   };
+
+  useEffect(() => {
+    startTransition(async () => {
+      // if (params) await fetchBjProblemById(parseInt(params, 10));
+      console.log(params);
+    });
+  }, []);
+
   return (
     <>
       <div className="px-4 py-[25px] md:px-8 md:py-[35px] lg:px-14 lg:py-[45px] xl:mx-auto xl:max-w-6xl xl:px-0">
         <div className="mb-[25px] md:mb-[35px]">
           <PageName title="알고리즘풀이" />
         </div>
+
         <EditText
           ref={editTextRef}
           tags={tags}
