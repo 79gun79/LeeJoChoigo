82b73ece75d6f8b0aca52c8e888daa0e8c9e6140
dlakrp123 <dlakrp731@gmail.com>
2025-06-16 14:51:00 +0900

    feat: add profileLinkNavigation component

---
+++ src/components/atoms/profileLinkNavigation.tsx
@@ -0,0 +1,11 @@
+export default function profileLinkNavigation() {
+  return (
+    <>
+      <img
+        src=""
+        alt=""
+        className="h-8 w-8 rounded-full object-cover md:h-10 md:w-10"
+      />
+    </>
+  );
+}
