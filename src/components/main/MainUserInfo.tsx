3adc4ed68de24d88a36388baae2323f2fa8c7963
Chosojeong <chosojeong97@gmail.com>
2025-06-17 00:00:55 +0900

    Fix : 각 채널의 문제 총 갯수와 푼 문제 갯수 표시 로직 변경

--- src/components/main/MainUserInfo.tsx
+++ src/components/main/MainUserInfo.tsx
@@ -1,12 +1,15 @@
 import { User } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useAuthStore } from '../../stores/authStore';
-import { getChannelProblems, getUserChannelPosts } from '../../api/mainApi';
+import {
+  getChannelProblems,
+  getChannelProblemsCount,
+  getUserChannelPosts,
+} from '../../api/mainApi';
 import type { User as UserType } from '../../types';
 import { Link, useNavigate } from 'react-router';
 
 type solvedPostType = { problem_id: number | null }[];
-type ProblemsType = Awaited<ReturnType<typeof getChannelProblems>>;
 
 export default function MainUserInfo({
   user,
@@ -20,35 +23,49 @@ export default function MainUserInfo({
   const session = useAuthStore((state) => state.session);
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
-  const [algorithmPost, setAlgorithmPost] = useState<ProblemsType>();
-  const [jobPost, setJobPost] = useState<ProblemsType>();
+  const [algorithmTotalCount, setAlgorithmTotalCount] = useState(0);
+  const [jobTotalCount, setJobTotalCount] = useState(0);
   const [solvedAlgorithm, setSolvedAlgorithm] = useState<solvedPostType>();
-  const [solvedJob, setSolvedJob] = useState<solvedPostType>();
+  const [solvedJob, setSolvedJob] = useState(0);
   useEffect(() => {
     const userInfoFetch = async () => {
       if (!session) return;
       setIsLoading(true);
       if (session) {
         try {
-          const algorithmPostData = await getChannelProblems(1);
+          // 채널의 문제들
           const jobPostData = await getChannelProblems(2);
 
-          setAlgorithmPost(algorithmPostData);
-          setJobPost(jobPostData);
+          // 문제 총 갯수
+          const algorithmCount = await getChannelProblemsCount(1);
+          const jobCount = await getChannelProblemsCount(2);
+
+          setAlgorithmTotalCount(algorithmCount || 0);
+          setJobTotalCount(jobCount || 0);
 
           if (user) {
+            // 푼 알고리즘 문제에서 중복으로 푼거 제외하기
             const solvedAlgorithmData = await getUserChannelPosts(user?.id, 3);
-            const solvedJobPostData = await getUserChannelPosts(user?.id, 4);
-
             const uniqueAlgorithm = solvedAlgorithmData
               ? uniquePostId(solvedAlgorithmData)
               : [];
-
-            const uniqueJob = solvedJobPostData
-              ? uniquePostId(solvedJobPostData)
-              : [];
             setSolvedAlgorithm(uniqueAlgorithm);
-            setSolvedJob(uniqueJob);
+
+            // 삭제된 개발직군 문제 리스트
+            const deleteJobProblems = jobPostData?.filter(
+              (f) => f.is_yn === false,
+            );
+
+            // 삭제 된 문제 있으면 푼 문제 배열에서 제거
+            if (deleteJobProblems) {
+              const solveJob = user.solved?.filter(
+                (f) => !deleteJobProblems.find((item) => item.id === f),
+              );
+              setSolvedJob(solveJob?.length || 0);
+            } else {
+              setSolvedJob(user.solved?.length || 0);
+            }
+
             setIsLoading(false);
           }
         } catch (error) {
@@ -106,7 +123,7 @@ export default function MainUserInfo({
                     <span className="text-4xl leading-none font-bold lg:text-5xl">
                       {solvedAlgorithm != null ? solvedAlgorithm?.length : 0}
                     </span>
-                    /<span>{algorithmPost?.length}</span>
+                    /<span>{algorithmTotalCount.toLocaleString()}</span>
                   </p>
                   <p className="rounded-2xl border border-[#ccc] px-3 py-0.5 text-xs md:px-4 md:text-sm lg:px-5 lg:text-base">
                     알고리즘 문제
@@ -119,9 +136,9 @@ export default function MainUserInfo({
                 >
                   <p className="flex items-end gap-0.5 md:text-lg">
                     <span className="text-4xl leading-none font-bold lg:text-5xl">
-                      {solvedJob != null ? solvedJob?.length : 0}
+                      {solvedJob}
                     </span>
-                    /<span>{jobPost?.length}</span>
+                    /<span>{jobTotalCount.toLocaleString()}</span>
                   </p>
                   <p className="rounded-2xl border border-[#ccc] px-3 py-0.5 text-xs md:px-4 md:text-sm lg:px-5 lg:text-base">
                     개발직군 문제
