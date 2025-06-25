4313224bb1ce69584cb2727cc44785e2596a0e82
79gun79 <79gun79@gmail.com>
2025-06-25 20:58:10 +0900

    zap: 또 수정

--- src/pages/solution/algorithm/AlgorithmSolutionEdit.tsx
+++ src/pages/solution/algorithm/AlgorithmSolutionEdit.tsx
@@ -209,7 +209,7 @@ export default function AlgorithmSolutionEdit() {
 
   return (
     <>
-      <div className="min-h-screen px-4 py-[25px] md:px-8 md:py-[35px] lg:h-[calc(100vh-64px)] lg:px-14 lg:py-[45px] xl:mx-auto xl:max-w-6xl xl:px-0">
+      <div className="min-h-screen px-4 py-[25px] md:px-8 md:py-[35px] lg:px-14 lg:py-[45px] xl:mx-auto xl:max-w-6xl xl:px-0">
         <div className="mb-[25px] md:mb-[35px]">
           <PageName title="알고리즘풀이" />
         </div>
